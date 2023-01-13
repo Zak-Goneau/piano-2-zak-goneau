b90e5d24e887904cd95e9329116b25bc19e82c08
Zak Goneau <118993110+Zak-Goneau@users.noreply.github.com>
2023-01-13 17:21:10 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,27 +1,99 @@
-basic.showString("Piano")
-basic.forever(function () {
+input.onButtonPressed(Button.A, function () {
     if (pins.digitalReadPin(DigitalPin.P6) == 1) {
-        music.playTone(262, music.beat(BeatFraction.Half))
+        music.playTone(131, music.beat(BeatFraction.Half))
     }
     if (pins.digitalReadPin(DigitalPin.P7) == 1) {
-        music.playTone(294, music.beat(BeatFraction.Half))
+        music.playTone(147, music.beat(BeatFraction.Half))
     }
     if (pins.digitalReadPin(DigitalPin.P8) == 1) {
-        music.playTone(330, music.beat(BeatFraction.Half))
+        music.playTone(165, music.beat(BeatFraction.Half))
     }
     if (pins.digitalReadPin(DigitalPin.P12) == 1) {
-        music.playTone(349, music.beat(BeatFraction.Half))
+        music.playTone(175, music.beat(BeatFraction.Half))
     }
     if (pins.digitalReadPin(DigitalPin.P13) == 1) {
-        music.playTone(392, music.beat(BeatFraction.Half))
+        music.playTone(196, music.beat(BeatFraction.Half))
     }
     if (pins.digitalReadPin(DigitalPin.P14) == 1) {
-        music.playTone(440, music.beat(BeatFraction.Half))
+        music.playTone(220, music.beat(BeatFraction.Half))
     }
     if (pins.digitalReadPin(DigitalPin.P15) == 1) {
-        music.playTone(494, music.beat(BeatFraction.Half))
-    }
-    if (pins.digitalReadPin(DigitalPin.P16) == 1) {
-        music.playTone(523, music.beat(BeatFraction.Half))
+        music.playTone(247, music.beat(BeatFraction.Half))
     }
 })
+input.onButtonPressed(Button.B, function () {
+    if (pins.digitalReadPin(DigitalPin.P6) == 1) {
+        music.playTone(523, music.beat(BeatFraction.Half))
+    }
+    if (pins.digitalReadPin(DigitalPin.P7) == 1) {
+        music.playTone(587, music.beat(BeatFraction.Half))
+    }
+    if (pins.digitalReadPin(DigitalPin.P8) == 1) {
+        music.playTone(659, music.beat(BeatFraction.Half))
+    }
+    if (pins.digitalReadPin(DigitalPin.P12) == 1) {
+        music.playTone(698, music.beat(BeatFraction.Half))
+    }
+    if (pins.digitalReadPin(DigitalPin.P13) == 1) {
+        music.playTone(784, music.beat(BeatFraction.Half))
+    }
+    if (pins.digitalReadPin(DigitalPin.P14) == 1) {
+        music.playTone(880, music.beat(BeatFraction.Half))
+    }
+    if (pins.digitalReadPin(DigitalPin.P15) == 1) {
+        music.playTone(988, music.beat(BeatFraction.Half))
+    }
+})
+input.onGesture(Gesture.Shake, function () {
+    for (let index = 0; index < 4; index++) {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # # # # #
+            . . # . .
+            . . # . .
+            `)
+    }
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # . # . #
+        # . . . #
+        # # # # #
+        `)
+})
+basic.showLeds(`
+    # # # # #
+    # . . . #
+    # . # . #
+    # . . . #
+    # # # # #
+    `)
+if (pins.digitalReadPin(DigitalPin.P6) == 1) {
+    music.playTone(262, music.beat(BeatFraction.Half))
+}
+if (pins.digitalReadPin(DigitalPin.P7) == 1) {
+    music.playTone(294, music.beat(BeatFraction.Half))
+}
+if (pins.digitalReadPin(DigitalPin.P8) == 1) {
+    music.playTone(330, music.beat(BeatFraction.Half))
+}
+if (pins.digitalReadPin(DigitalPin.P12) == 1) {
+    music.playTone(349, music.beat(BeatFraction.Half))
+}
+if (pins.digitalReadPin(DigitalPin.P13) == 1) {
+    music.playTone(392, music.beat(BeatFraction.Half))
+}
+if (pins.digitalReadPin(DigitalPin.P14) == 1) {
+    music.playTone(440, music.beat(BeatFraction.Half))
+}
+if (pins.digitalReadPin(DigitalPin.P15) == 1) {
+    music.playTone(494, music.beat(BeatFraction.Half))
+}
